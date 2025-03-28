5e73703e2e91a72fa1dc7ebe12992aee1da295a9
unknown <antoine.cuvilliez@outlook.fr>
2025-03-28 11:59:18 +0100

    feat(app) Remove console.log

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,12 +1,5 @@
 import { DataSource } from 'typeorm';
 
-console.log('🌱 ENV CONFIG DATA SOURCE:');
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
-console.log('DB_USER:', process.env.DB_USERNAME);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
-
 export default new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
--- src/database/databas.module.ts
+++ src/database/databas.module.ts
@@ -1,13 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-console.log('🌱 ENV CONFIG DATABASE MODULE :');
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
-console.log('DB_USER:', process.env.DB_USERNAME);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
-
 @Module({
   imports: [
     TypeOrmModule.forRoot({
